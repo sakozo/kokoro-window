322c2e07461ea517cc205c0dedcfa67bc96168ac
sakozo <youlianggit@gmail.com>
2020-05-02 22:34:21 +0900

    register icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,25 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
   end
 
   def update
-    if current_user.update(user_params) #←処理が実行されたらtrue、実行されなければfalseを返します。
-      redirect_to root_path   #←updateができたらroot_pathにリダイレクトする。（ビューの方でSubmitになっているのでlink_toとか無理やり遷移先付けるより、コントローラーで処理してやったほうが条件分岐とかできるのでいいのかも）
+    user = User.find(current_user.id)
+    if user.update(user_params)
+      redirect_to user_path
     else
-      render :edit  #←デフォルトでは、”app/views/コントローラー名/メソッド名.html.erb”という名前のファイルを呼び出します。　今回はupdateに失敗したら :editなのでedit.html.hamlのビューを呼び出すようにする
+      render new_user_path
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :icon)
   end
 end
